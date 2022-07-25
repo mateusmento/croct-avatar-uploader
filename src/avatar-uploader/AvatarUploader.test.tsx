59b23dbd86bf1f6ec513f859ebb3596924e61987
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-07-24 22:31:06 -0300

    Add test for displaying selected file as avatar image

--- src/avatar-uploader/AvatarUploader.test.tsx
+++ src/avatar-uploader/AvatarUploader.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import { AvatarUploader } from './AvatarUploader';
+import userEvent from '@testing-library/user-event';
 
 test('renders AvatarUploader', () => {
 	render(<AvatarUploader />);
@@ -10,4 +11,22 @@ test('renders AvatarUploader initial layout', () => {
 	render(<AvatarUploader />);
 	screen.getByText(/Organization Logo/i);
 	screen.getByText(/Drop the image here or click to browse./i);
+});
+
+const readFile = (file: File) => new Promise<string>((resolve, reject) => {
+	const reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = () => resolve(reader.result as string);
+	reader.onerror = error => reject(error);
+});
+
+test('displays selected file as avatar image', async () => {
+	render(<AvatarUploader />);
+	let fileInput = screen.getByTestId("file-input");
+	let file = new File(["image"], "image.png", { type: "image/png" });
+	userEvent.upload(fileInput, file);
+	await waitFor(async () => {
+		let preview: HTMLImageElement = screen.getByTestId('avatar-preview');
+		expect(preview.src).toBe(await readFile(file));
+	});
 });
