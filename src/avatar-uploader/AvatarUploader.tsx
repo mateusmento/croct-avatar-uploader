5374bfa95d1a11a6568e93add684dc8212b7b953
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-07-24 21:56:41 -0300

    Add styling to avatar uploader root element

--- src/avatar-uploader/AvatarUploader.tsx
+++ src/avatar-uploader/AvatarUploader.tsx
@@ -1,8 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { BsImage } from 'react-icons/bs';
 
 export function AvatarUploader() {
 	return (
-		<div>
+		<div css={avatarUploaderCss}>
 			<span>
 				<BsImage/>
 				Organization Logo
@@ -11,3 +13,21 @@ export function AvatarUploader() {
 		</div>
 	);
 }
+
+let avatarUploaderCss = css`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	margin: auto;
+
+	background-color: #f2f5f8;
+	color: #555;
+	width: 600px;
+	height: 200px;
+
+	background-image: repeating-linear-gradient(0deg, #c7cdd3, #c7cdd3 9px, transparent 9px, transparent 13px, #c7cdd3 13px), repeating-linear-gradient(90deg, #c7cdd3, #c7cdd3 9px, transparent 9px, transparent 13px, #c7cdd3 13px), repeating-linear-gradient(180deg, #c7cdd3, #c7cdd3 9px, transparent 9px, transparent 13px, #c7cdd3 13px), repeating-linear-gradient(270deg, #c7cdd3, #c7cdd3 9px, transparent 9px, transparent 13px, #c7cdd3 13px);
+	background-size: 3px 100%, 100% 3px, 3px 100% , 100% 3px;
+	background-position: 0 0, 0 0, 100% 0, 0 100%;
+	background-repeat: no-repeat;
+`;
