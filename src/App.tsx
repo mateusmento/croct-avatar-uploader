2826bcb237fa448c2c548271bbfc68df6926c868
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-07-24 22:38:32 -0300

    Add AvatarUploader to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { AvatarUploader } from './avatar-uploader/AvatarUploader';
 
 function App() {
   return (
     <div className="App">
+			<AvatarUploader/>
     </div>
   );
 }
