012fc58763ebc37fff1a1635f48f4fe750a7b824
Mateus Sarmento <mateussarmento@blumar.com.br>
2022-07-24 21:37:10 -0300

    Define AvatarUploader functional component

---
+++ src/avatar-uploader/AvatarUploader.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { AvatarUploader } from './AvatarUploader';
+
+test('renders AvatarUploader', () => {
+	render(<AvatarUploader />);
+});
